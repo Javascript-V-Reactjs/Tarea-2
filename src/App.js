4943a6d50b4a9e0a329100ac790ec3e7095c1713
Ignacio Cl <juanigcl2532@gmail.com>
2021-10-20 13:06:31 -0600

    Adding 3rd homework

--- src/App.js
+++ src/App.js
@@ -1,21 +1,93 @@
 import './App.css';
-import { students } from './students';
-import List from './components/List'
-import Navbar from './components/Navbar';
-import Layout from './components/Layout'
-import Footer from './components/Footer';
-import Rows  from './titles';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Layout from "./components/Layout"
+import List from "./components/List"
+import Footer from "./components/Footer"
 
-function App() {
-  return (
-    <Layout>
-      <Navbar/>
-      <List students={students}>
-        <Rows/>
-      </List>
-      <Footer/>
-    </Layout>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      students: [],
+      isLoaded: false,
+      id: 0,
+      name: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    fetch("https://students.hasura.app/api/rest/students", {
+      method: "GET",
+      headers: {
+        "x-hasura-admin-secret":
+          "733M3Tgq5IK2ALRXFSivpX86TGJX82goni63azRwZGCtVY1qN4t8521f1LE4iKxq"
+      }
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({
+          students: result.students,
+          isLoaded: true
+        })
+      })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const id = this.state.id
+    const name = this.state.name
+    const students = this.state.students
+    this.setState({
+      id: '',
+      name:''
+    });
+    for(let i = 0; i < students.length; i++){
+      if(Number(id) === students[i].id){
+        return alert('El id especificado ya existe en nuestro sistema');
+      }
+    }
+    const newStudentsArray = [...students, { id: id, name: name }]
+    this.setState({ students: newStudentsArray })
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    const { students, isLoaded } = this.state
+    return (
+      <Layout>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            required
+            name="id"
+            onChange={this.handleChange}
+            value={this.state.id}
+            type="number"
+            placeholder="123"
+          ></input>
+          <input
+            required
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+            type="text"
+            placeholder="Jon Snow"
+          ></input>
+          <button type="submit">Add Student</button>
+        </form>
+        <Navbar />
+        {!isLoaded ? <p>Loading...</p> : <List hoverable students={students} />}
+        <Footer />
+      </Layout>
+    )
+  }
 }
 
-export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,35 +1,27 @@
-import {useState} from 'react'
+import React from "react"
+
 function List(props) {
-    const [hoverable, setHoverStatus] = useState(false);
-    const headerTitles = props.children.type;
     return (
-        <div style={{flex: "1 0 auto"}}>
+        <div>
             <table>
                 <thead>
-                    <tr>
-                        {
-                        headerTitles.map(title =>
-                            <th key ={title.id}>
-                                {title.header}
-                            </th>
-                        )
-                        }
-                    </tr>
+                    <th>id</th>
+                    <th>Name</th>
+                    <th>Is present ?</th>
                 </thead>
                 <tbody>
-                    {
-                        props.students.map(student => 
-                            <tr key={student.id} onMouseEnter ={() => setHoverStatus(true)} onMouseLeave ={() => setHoverStatus(false)}
-                            style={{
-                                border: hoverable ? '1px solid var(--highlight)': null
-                            }}
-                            >
-                                <td>{student.id}</td>
-                                <td>{student.name}</td>
-                                <td><input type="checkbox" style={{marginLeft:"auto"}}/></td>
-                            </tr>
-                        )
-                    }
+                    {props.students.map((currentStudent) => (
+                        <tr
+                            className={props.hoverable ? "hoverable" : ""}
+                            key={currentStudent.id}
+                        >
+                            <td>{currentStudent.id}</td>
+                            <td>{currentStudent.name}</td>
+                            <td>
+                                <input type="checkbox" />
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
