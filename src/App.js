229523f2756c8fc81ceecd84052594aa69042bfa
Jean Brown <jeantoruno00@gmail.com>
2021-10-28 18:47:11 -0700

    Tarea-3

--- src/App.js
+++ src/App.js
@@ -1,80 +1,97 @@
 import './App.css';
-import React from 'react';
-// import { students } from './students';
+import React, { useState, useEffect } from 'react';
 import List from './components/List'
 import Navbar from './components/Navbar';
 import Layout from './components/Layout'
 import Footer from './components/Footer';
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      students: {},
-      isLoaded: false,
-      id: 0,
-      name: ''
-    }
-    // this.handleIdChange = this.handleIdChange.bind(this);
-    // this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function App() {
+  const [idInput, setIdInput] = useState(0)
+  const [nameInput, setNameInput] = useState('')
+  const [studentsList, setStudentsList] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [alert, setAlert] = useState(false)
+  const [disable, setDisabled] = useState(false)
 
-  componentDidMount() {
-    fetch('https://students.hasura.app/api/rest/students', {
+
+  useEffect(() => {
+    fetchStudents()
+  }, [alert])
+
+  const fetchStudents = () => {
+    return fetch('https://students.hasura.app/api/rest/students', {
       method: 'GET',
       headers: {
         'x-hasura-admin-secret': '733M3Tgq5IK2ALRXFSivpX86TGJX82goni63azRwZGCtVY1qN4t8521f1LE4iKxq'
       }
     }).then(response => response.json())
       .then(result => {
-        this.setState({
-          students: result.students,
-          isLoaded: true
+        setStudentsList(result.students)
+        setIsLoaded(true)
       })
-    })
   }
 
-  handleSubmit(event) {
-    const id = this.state.id
-    const name = this.state.name
+  const insertStudent = (id, name) => {
+    return fetch('https://students.hasura.app/api/rest/student', {
+      method: 'POST',
+      headers: {
+        'x-hasura-admin-secret': '733M3Tgq5IK2ALRXFSivpX86TGJX82goni63azRwZGCtVY1qN4t8521f1LE4iKxq'
+      },
+      body: JSON.stringify(
+        {
+          'id': id,
+          'name': name
+        })
+    }).then(response => response.json())
+      .catch(error => console.log(error))
+  }
+
+  const formValidation = (event) => {
     event.preventDefault()
-    const students = [...this.state.students, {"id": id, "name": name}]
-    this.setState({students: students})
+    if (nameInput === '') {
+      setNameInput('El campo está vacio')
+      setDisabled(true)
+    } else if (idInput === 0) {
+      setDisabled(true)
+    } else {
+      insertStudent(idInput, nameInput).then(() => {
+        setAlert(true)
+        setIdInput(0)
+        setNameInput("")
+        setDisabled(false)
+      })
+    }
   }
 
-  // handleIdChange(event) {
-  //   this.setState({id: event.target.value});  
-  // }
-
-  // handleNameChange(event) {
-  //   this.setState({name: event.target.value});  
-  // }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  render(){
-    const { students, isLoaded } = this.state
-    return (
-      <Layout>
-        <Navbar/>
-
-        <form onSubmit={this.handleSubmit}>
-          <input value={this.state.id} type="number" name="id" placeholder="id" onChange={this.handleChange}></input>
-          <input value={this.state.name} type="text" name="name" placeholder="name" onChange={this.handleChange}></input>
-          <button type="submit" value="submit">Add student</button>
-        </form>
-        
-        {!isLoaded ? <p>loading...</p> : <List students={students} hoverable/>}
-        <Footer />
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <form onSubmit={formValidation}>
+        <input
+          value={idInput}
+          type="number"
+          name="id"
+          placeholder="123"
+          onChange={e => setIdInput(e.target.value)}>
+        </input>
+        <input
+          value={nameInput}
+          type="text"
+          name="name"
+          placeholder="name"
+          onChange={e => setNameInput(e.target.value)}>
+        </input>
+        {alert && <h2>Nuevo estudiante ingresado</h2>}
+        <button type="submit" disabled={disable} value="submit">Add student</button>
+      </form>
+      <Navbar />
+      {!isLoaded ? (
+        <p>Loading...</p>
+      ) : (
+        <List students={studentsList} hoverable />
+      )}
+      <Footer />
+    </Layout>
+  );
 }
 
 export default App;
