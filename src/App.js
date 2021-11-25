53cdb9c4f7596990abe165ff22ed3f90826fae97
AlexGuzman98 <alexd.guzman98@gmail.com>
2021-11-25 11:49:01 -0600

    tarea5

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+import StudentList from './routes/StudentList'
+import Navbar from './components/Navbar';
+import Layout from './components/Layout'
+import Footer from './components/Footer';
+import StudentForm from './routes/StudentForm';
+import { Routes, Route } from 'react-router-dom'
+import Student from './routes/Student';
+import AlertTag from './components/AlertTag';
+import useAlert from './hooks/useAlert';
+
+export default function App() {
+  const { type, message, isOpened } = useAlert();
+
+  return (
+    <Layout>
+      <Navbar />
+      <AlertTag isOpened={isOpened} type={type} message={message} />
+      <Routes>
+        <Route path="/" element={<StudentList hoverable />} />
+        <Route path="/addStudent" element={<StudentForm />} />
+        <Route path="/student/:StudentId" element={<Student />} />
+      </Routes>
+
+      <Footer />
+    </Layout>
+  );
+}
---
+++ src/hooks/useAlert.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useContext, useState, useEffect, createContext } from "react";
+import AlertTag from "../components/AlertTag";
+
+export const AlertContext = createContext();
+
+export const AlertProvider = ({ children }) => {
+    const [message, setMessage] = useState("");
+    const [type, setType] = useState("");
+    const [isOpened, setIsOpened] = useState(false);
+    const [autoClose, setAutoClose] = useState(5000)
+    const [position, setPosition] = useState("right")
+
+    const sendAlert = ({ type, message, autoClose, position }) => {
+        setMessage(message);
+        setType(type);
+        setIsOpened(true);
+        setPosition(!position ? 'rigth' : position)
+        setAutoClose(!autoClose ? 5000 : autoClose)
+    };
+
+    useEffect(() => {
+        const alertTimer = setTimeout(() => {
+            setIsOpened(false)
+        }
+            , autoClose)
+        return () => clearTimeout(alertTimer)
+    })
+
+    return (
+        <AlertContext.Provider value={{ sendAlert, type, message, isOpened, position }}>
+            {children}
+        </AlertContext.Provider>
+    );
+};
+
+const useAlert = () => {
+    const context = useContext(AlertContext);
+
+    if (context === undefined) {
+        throw new Error(
+            "You must wrap your application with <AlertProvider /> in order to use useAlert hook."
+        );
+    }
+
+    return context;
+};
+
+export default useAlert;
