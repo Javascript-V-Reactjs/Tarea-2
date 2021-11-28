b2893aa64f1f7ee77a6caff6df827ce01c77aa6f
JamesSoFer <solisjr1919@gmail.com>
2021-11-28 01:18:31 -0600

    Agrega cambios en tarea 4

--- src/hooks/useAlert.js
+++ src/hooks/useAlert.js
@@ -8,25 +8,28 @@ export const AlertProvider = ({ children }) => {
     const [message, setMessage] = useState('')
     const [type, setType] = useState('')
     const [isOpened, setIsOpenned] = useState(false)
+    const [position, setPosition] = useState('right')
+    const [closeCount, setCloseCount] = useState(10000)
     
-    const sendAlert = ({type, message}) => {
+    const sendAlert = ({type, message, closeCount, position}) => {
         setMessage(message)
         setType(type)
         setIsOpenned(true)
+        setCloseCount(!closeCount ? 10000:closeCount)
+        setPosition(!position ? 'right':position)
     }
-
-    
 
     useEffect(() => {
         const alertTimer = setTimeout(()=> {
             setIsOpenned(false)
+        }
+        , closeCount)
         return () => clearTimeout(alertTimer)
-        }, 5000)
         
     })
 
     return (
-        <AlertContext.Provider value={{ sendAlert, type, message, isOpened, AlertTag}}>
+        <AlertContext.Provider value={{ sendAlert, type, message, isOpened, position}}>
             { children }
         </AlertContext.Provider>
     )
@@ -38,7 +41,7 @@ const useAlert = () => {
     if (context === undefined) {
         throw new Error(
             'You must wrap your application with <AlertProvider /> in order to useAlert().',
-          ) 
+        ) 
     }
     return context
 }
